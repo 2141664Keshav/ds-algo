916092e2efbb5a885b355cb0dcb4b011757f794d
Keshav Dutt Kaushik <keshav.duttkaushik@cognizant.com>
2025-12-11 15:53:48 +0530

    first commit

---
+++ src/main/java/org/cognizant/m_0_intro_to_prob_1/lec_01_intro/assignment/q2_isPrime/Solution.java
@@ -0,0 +1,14 @@
+package org.cognizant.m_0_intro_to_prob_1.lec_01_intro.assignment.q2_isPrime;
+
+public class Solution {
+
+    public int solve(int A) {
+        if (A == 1) return 0;
+
+        for (int i = 2; i * i <= A; i++) {
+            if (A % i == 0) return 0;
+        }
+
+        return 1;
+    }
+}
